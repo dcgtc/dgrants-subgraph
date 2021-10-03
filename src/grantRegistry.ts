b969e4cb24a41f2cd9a9602e799701e1bd97488d
Graham Dixon <graham@gdixon.co.uk>
2021-10-03 05:11:45 +0100

    feat: adds support for contract events and sets up deploy for rinkeby/polygon

---
+++ src/grantRegistry.ts
@@ -0,0 +1,47 @@
+import {
+  GrantCreated,
+  GrantUpdated
+} from "../generated/GrantRegistry/GrantRegistry"
+import { Grant } from "../generated/schema"
+
+export function handleGrantCreated(event: GrantCreated): void {
+    // Entities can be loaded from the store using a string ID; this ID
+    // needs to be unique across all entities of the same type
+    let entity = Grant.load(event.params.id.toHex())
+
+    // Entities only exist after they have been saved to the store;
+    // `null` checks allow to create entities on demand
+    if (entity == null) {
+        entity = new Grant(event.params.id.toHex())
+    }
+
+    // Entity fields can be set based on event parameters
+    entity.id = event.params.id.toHex()
+    entity.owner = event.params.owner
+    entity.payee = event.params.payee
+    entity.metaPtr = event.params.metaPtr
+
+    // Entities can be written to the store with `.save()`
+    entity.save()
+}
+
+export function handleGrantUpdated(event: GrantUpdated): void {
+    // Entities can be loaded from the store using a string ID; this ID
+    // needs to be unique across all entities of the same type
+    let entity = Grant.load(event.params.id.toHex())
+
+    // Entities only exist after they have been saved to the store;
+    // `null` checks allow to create entities on demand
+    if (entity == null) {
+        entity = new Grant(event.params.id.toHex())
+    }
+
+    // Entity fields can be set based on event parameters
+    entity.id = event.params.id.toHex()
+    entity.owner = event.params.owner
+    entity.payee = event.params.payee
+    entity.metaPtr = event.params.metaPtr
+
+    // Entities can be written to the store with `.save()`
+    entity.save()
+}
